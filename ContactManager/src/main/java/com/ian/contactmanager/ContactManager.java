fa02e06ce269dc312f513dec1e7bbea3b0a01602
iankristoper <iankristoper26@gmail.com>
2024-11-17 23:56:20 +0800

    initial commit

--- ContactManager/src/main/java/com/ian/contactmanager/ContactManager.java
+++ ContactManager/src/main/java/com/ian/contactmanager/ContactManager.java
@@ -1,16 +1,95 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
 package com.ian.contactmanager;
 
-/**
- *
- * @author Ian Tagano
- */
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+
+
+
+//class for the data of the contact manager
+class ContactData {
+    
+    private String name;
+    private String number;
+    private String email;
+    
+    //contructor for the class [ContactData] for initialization of data
+    public ContactData(String name, String number, String email) {
+        
+        this.name = name;
+        this.number = number;
+        this.email = email;
+        
+    }
+    
+    //getters
+    public String getName() {
+        return name;
+    }
+    
+    public String getNumber() {
+        return number;
+    }
+    
+    public String getEmail() {
+        return email;
+    }
+    
+    //setters
+    public void setName(String name) {
+        this.name = name;
+    }
+    
+    public void setNumber(String number) {
+        this.number = number;
+    }
+    
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    
+    //method overriding 
+    @Override
+    public String toString() {
+        return "sample only!";
+    }
+    
+}
+
+
+//class for contact functions 
+class ContactFunctions {
+    
+    Scanner scanner = new Scanner(System.in);
+    
+    
+    
+   
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 public class ContactManager {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
+        
     }
 }
