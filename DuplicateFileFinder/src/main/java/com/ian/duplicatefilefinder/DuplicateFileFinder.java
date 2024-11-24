32edb931886ea7a98b8f73d44e7842c75d35f548
iankristoper <iankristoper26@gmail.com>
2024-11-24 22:02:06 +0800

    initial commit

---
+++ DuplicateFileFinder/src/main/java/com/ian/duplicatefilefinder/DuplicateFileFinder.java
@@ -0,0 +1,31 @@
+package com.ian.duplicatefilefinder;
+
+
+/*
+    Description:
+    Write a program to find duplicate files in a directory by comparing file names or content (basic comparison, not full hashing).
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class DuplicateFileFinder {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ FrequencyWordCounter/src/main/java/com/ian/frequencywordcounter/FrequencyWordCounter.java
@@ -0,0 +1,28 @@
+package com.ian.frequencywordcounter;
+
+
+
+/*
+    Description:
+    Create a program that reads a string of text from the user and counts the frequency of each word.
+*/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+public class FrequencyWordCounter {
+
+    public static void main(String[] args) {
+        
+    }
+}
